96c88e0a1fc871f354b0e0c8b2486006b03e2f0e
Andres <andreseguanipa1@gmail.com>
2021-10-19 22:20:41 -0400

    CASI listo para produccion

--- src/config/config.js
+++ src/config/config.js
@@ -9,3 +9,44 @@ SEED de Autenticación
 ======================= */
 
 process.env.SEED = process.env.SEED || 'token-SEED'
+
+
+/* =========
+Puerto
+========= */
+
+process.env.PORT = process.env.PORT || 3000;
+
+
+/* =========
+Entorno
+========= */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/* ====================
+BASE DE DATOS
+======================= */
+
+let dbUser;
+let dbPassword;
+let dbHost;
+let dbName;
+
+if (process.env.NODE_ENV === 'dev') {
+    dbName = 'lagomall';
+    dbUser = 'root';
+    dbPassword = '';
+    dbHost = 'localhost'
+} else {
+    dbName = 'epiz_30111988_lagotest';
+    dbUser = 'epiz_30111988';
+    dbPassword = 'y1ONARfdkaJG3gI';
+    dbHost = 'sql305.epizy.com';
+}
+
+process.env.DBNAME = dbName;
+process.env.DBUSER = dbUser;
+process.env.DBPASSWORD = dbPassword;
+process.env.DBHOST = dbHost;
