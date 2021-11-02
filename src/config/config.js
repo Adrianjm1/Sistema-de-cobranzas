3fc18d9f07792051972530852a9dce13a1498702
unknown <adrianmolina39@gmail.com>
2021-11-01 22:14:31 -0400

    Commit

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 if (process.env.NODE_ENV === 'dev') {
 
-    process.env.DBNAME = 'lagomall';
-    process.env.DBUSER = 'root';
-    process.env.DBPASSWORD = '';
-    process.env.DBHOST = 'localhost';
+    process.env.DBNAME = 'centrode_lagomall';
+    process.env.DBUSER = 'centrode_admin';
+    process.env.DBPASSWORD = 'Lagomall2021';
+    process.env.DBHOST = 'host.hostingmaracaibo.com';
     process.env.SEED = 'token-SEED';
     process.env.CADUCIDAD_TOKEN = '5h';
 
 }
+
+// process.env.DBNAME = 'centrode_lagomall';
+// process.env.DBUSER = 'centrode_administrador';
+// process.env.DBPASSWORD = 'Lagomall2021';
+// process.env.DBHOST = 'host.hostingmaracaibo.com';
+// process.env.SEED = 'token-SEED';
+// process.env.CADUCIDAD_TOKEN = '5h';
