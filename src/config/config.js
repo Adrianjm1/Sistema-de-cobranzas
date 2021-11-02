3331537cf66a51286936501fb1fe8a3c6f90728a
Andres <andreseguanipa1@gmail.com>
2021-11-01 21:52:44 -0400

    listo

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV == 'dev') {
 
     process.env.DBNAME = 'lagomall';
     process.env.DBUSER = 'root';
