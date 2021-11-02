25146c600d4273bb96314e34ad87af90e5174d48
unknown <adrianmolina39@gmail.com>
2021-11-01 22:25:02 -0400

    fixing

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV == 'dev') {
     process.env.DBNAME = 'centrode_lagomall';
     process.env.DBUSER = 'centrode_admin';
     process.env.DBPASSWORD = 'Lagomall2021';
-    process.env.DBHOST = 'host.hostingmaracaibo.com';
+    process.env.DBHOST = '138.128.182.50';
     process.env.SEED = 'token-SEED';
     process.env.CADUCIDAD_TOKEN = '5h';
 
